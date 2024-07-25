6720aa3d2defc659c51e7dcd1e1c55a9904080b4
Muhammad Subhan <112649712+subhan-tahir@users.noreply.github.com>
2024-07-25 09:44:09 +0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import Navbar from './Components/Navbar'
-import Home from './Components/Home'
+import Carloan from './Carloan'
+import carBg from './assets/carbg.jpg'
+
 
 const App = () => {
   return (
     <>
-    <Navbar />
-    <Home />
+    <div className='w-full h-screen'>
+    <img src={carBg} alt="" className='object-cover w-full h-full'/>
+         <Carloan />
+   </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Provider } from 'react-redux'
-import store from './Store/idex.js'
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-    </BrowserRouter>
-  </Provider>
+  </React.StrictMode>,
 )
